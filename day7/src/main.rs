3599dd2ce175706963eeb0de8398bbe6744a971e
Sriharsha Mucheli <harshasrisri@gmail.com>
2019-02-21 16:34:55 -0800

    hashmap model. ext trying petgraph

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,3 +1,13 @@
+#[macro_use] extern crate text_io;
+use std::io::{BufRead, BufReader};
+use std::collections::{HashMap,HashSet};
+
 fn main() {
-    println!("Hello, world!");
+    let mut deps = HashMap::new();
+    for line in BufReader::new(std::io::stdin()).lines().filter_map(|r| r.ok()) {
+        let (src, dst) : (char, char);
+        scan!(line.bytes() => "Step {} must be finished before step {} can begin.", src, dst);
+        deps.entry(dst).or_insert(Vec::new()).push(src);
+    }
+    println!("{:?}", deps);
 }
