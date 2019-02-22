409ff31f9765e597b48bd24550ed62313434e3a9
Sriharsha Mucheli <harshasrisri@gmail.com>
2019-02-21 17:05:56 -0800

    di graph map using petgraph

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,13 +1,15 @@
 #[macro_use] extern crate text_io;
+extern crate petgraph;
+use petgraph::graphmap::DiGraphMap;
+use petgraph::dot::{Dot, Config};
 use std::io::{BufRead, BufReader};
-use std::collections::{HashMap,HashSet};
 
 fn main() {
-    let mut deps = HashMap::new();
+    let mut deps = DiGraphMap::new();
     for line in BufReader::new(std::io::stdin()).lines().filter_map(|r| r.ok()) {
         let (src, dst) : (char, char);
         scan!(line.bytes() => "Step {} must be finished before step {} can begin.", src, dst);
-        deps.entry(dst).or_insert(Vec::new()).push(src);
+        deps.add_edge (src, dst, deps.edge_count());
     }
-    println!("{:?}", deps);
+    println!("{:?}", Dot::with_config(&deps, &[Config::EdgeIndexLabel]));
 }
