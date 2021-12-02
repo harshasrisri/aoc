1ac888ee51d8523300ed6388cc34d62bbd383ef9
Sriharsha Mucheli <harshasrisri@gmail.com>
2021-12-02 15:02:42 -0800

    day2 part1

--- 2021/src/main.rs
+++ 2021/src/main.rs
@@ -27,6 +27,33 @@ fn day1() {
     println!("{}, {}", d1p1, d1p2);
 }
 
+fn day2() {
+    let file = File::open("inputs/d02.txt").unwrap();
+    let reader = BufReader::new(file);
+
+    let movements = reader
+        .lines()
+        .filter_map(|line| line.ok())
+        .map(|line| {
+            if let Some((dir, steps)) = line.split_once(' ') {
+                let steps = steps.parse::<isize>().unwrap();
+                match dir {
+                    "forward" => (steps, 0),
+                    "down" => (0, steps),
+                    "up" => (0, -1 * steps),
+                    _ => (0, 0),
+                }
+            } else { (0,0) }
+        })
+    .collect::<Vec<(isize, isize)>>();
+
+    let distance: isize = movements.iter().map(|t| t.0).sum();
+    let depth: isize  = movements.iter().map(|t| t.1).sum();
+
+    println!("{}, {}, {}", distance, depth, distance * depth);
+}
+
 fn main() {
     day1();
+    day2();
 }
