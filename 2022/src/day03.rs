e1f114c4cf6311a4c859a911cb9a87f358ad2ab4
Sriharsha Mucheli <harshasrisri@gmail.com>
2022-12-02 21:43:48 -0800

    Day 3 part 2

--- 2022/src/day03.rs
+++ 2022/src/day03.rs
@@ -19,7 +19,32 @@ pub fn run(input: &'static str) -> (usize, usize) {
                     0
                 }
         });
-    (p1.into(), 0)
+
+    let p2 = input
+        .lines()
+        .collect::<Vec<_>>()
+        .chunks(3)
+        .map(|chunk| {
+            let c1 = chunk[0].chars().to_owned().collect::<HashSet<_>>();
+            let c2 = chunk[1].chars().to_owned().collect::<HashSet<_>>();
+
+            let c12 = c1.intersection(&c2).copied().collect::<HashSet<_>>();
+            let c3 = chunk[2].chars().to_owned().collect::<HashSet<_>>();
+            *c3.intersection(&c12).next().unwrap() as usize
+        })
+    .fold(0, |acc, x| {
+        acc +
+            if x >= 'a' as usize && x <= 'z' as usize {
+                x - 'a' as usize + 1
+            } else if x >= 'A' as usize && x <= 'Z' as usize {
+                x - 'A' as usize + 1 + 26
+            } else {
+                0
+            }
+    });
+
+
+    (p1, p2)
 }
 
 #[test]
@@ -32,5 +57,5 @@ wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
 ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw
 ";
-    assert_eq!(run(input), (157,0));
+    assert_eq!(run(input), (157,70));
 }
