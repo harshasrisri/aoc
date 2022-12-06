eb4b22a10ef33f78530dde2d6f26169480596245
Sriharsha Mucheli <harshasrisri@gmail.com>
2022-12-05 21:41:49 -0800

    Day 6 part 2 refactor

--- 2022/src/day06.rs
+++ 2022/src/day06.rs
@@ -3,19 +3,17 @@ use std::collections::HashSet;
 pub fn run(input: &'static str) -> (usize, usize) {
     let find_start = |win_size: usize| -> usize {
         input
-        .as_bytes()
-        .windows(win_size)
-        .enumerate()
-        .find_map(|(pos, bytes)| {
-            eprintln!("{}, {:?}", pos, bytes);
-            let bytes = bytes.iter().collect::<HashSet<_>>();
-            if bytes.len() == win_size {
-                Some(pos + win_size)
-            } else {
-                None
-            }
-        })
-        .unwrap()
+            .as_bytes()
+            .windows(win_size)
+            .enumerate()
+            .find_map(|(pos, bytes)| {
+                if win_size == bytes.iter().collect::<HashSet<_>>().len() {
+                    Some(pos + win_size)
+                } else {
+                    None
+                }
+            })
+            .unwrap()
     };
 
     (find_start(4), find_start(14))
