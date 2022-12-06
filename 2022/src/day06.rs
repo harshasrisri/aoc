c56b9e3007872e7a6e1b711310d85099e7625880
Sriharsha Mucheli <harshasrisri@gmail.com>
2022-12-05 21:12:00 -0800

    Day 6 part 2

--- 2022/src/day06.rs
+++ 2022/src/day06.rs
@@ -16,11 +16,26 @@ pub fn run(input: &'static str) -> (usize, usize) {
         })
         .unwrap();
 
-    (p1, 0)
+    let p2 = input
+        .as_bytes()
+        .windows(14)
+        .enumerate()
+        .find_map(|(pos, bytes)| {
+            eprintln!("{}, {:?}", pos, bytes);
+            let bytes = bytes.iter().collect::<HashSet<_>>();
+            if bytes.len() == 14 {
+                Some(pos + 14)
+            } else {
+                None
+            }
+        })
+        .unwrap();
+
+    (p1, p2)
 }
 
 #[test]
 fn test() {
     let input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
-    assert_eq!(run(input), (7,0));
+    assert_eq!(run(input), (7, 19));
 }
