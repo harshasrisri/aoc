9fc600f5d963f39d314642a6f369f253e55bc319
Sriharsha Mucheli <harshasrisri@gmail.com>
2022-12-06 22:10:44 -0800

    Day 7 part 2 refactor

--- 2022/src/day07.rs
+++ 2022/src/day07.rs
@@ -1,37 +1,34 @@
-use std::{collections::HashMap, path::PathBuf};
 use sscanf::sscanf;
+use std::{collections::HashMap, path::PathBuf};
 
 pub fn run(input: &'static str) -> (usize, usize) {
     let mut pwd = PathBuf::new();
     let mut dir_sizes: HashMap<PathBuf, usize> = HashMap::new();
 
     for line in input.lines() {
-        // eprintln!("{}({}): {line}", pwd.display(), dir_sizes.get(&pwd).unwrap_or(&0));
         if line.starts_with("$ cd") {
             match line.splitn(3, ' ').last().unwrap() {
-                ".." => { pwd.pop(); }
-                child => { pwd.push(child); }
+                ".." => {
+                    pwd.pop();
+                }
+                child => {
+                    pwd.push(child);
+                }
             };
-        } else if line.starts_with("$ ls") {
-            continue;
-        } else if line.starts_with("dir") {
+        } else if line.starts_with("$ ls") || line.starts_with("dir") {
             continue;
         } else {
             let (size, _file) = sscanf!(line, "{} {}", usize, str).unwrap();
-            pwd
-                .ancestors()
-                .for_each(|anc| {
-                    dir_sizes.entry(anc.to_path_buf()).and_modify(|sz| *sz += size).or_insert(size);
-                });
+            pwd.ancestors().for_each(|anc| {
+                dir_sizes
+                    .entry(anc.to_path_buf())
+                    .and_modify(|sz| *sz += size)
+                    .or_insert(size);
+            });
         }
     }
 
-    let p1 = dir_sizes
-        .values()
-        // .inspect(|v| eprintln!("{v}"))
-        .filter(|v| **v < 100000)
-        // .inspect(|v| eprintln!("{v}"))
-        .sum();
+    let p1 = dir_sizes.values().filter(|v| **v < 100000).sum();
 
     let mut sorted_sizes = dir_sizes.values().collect::<Vec<_>>();
     sorted_sizes.sort();
