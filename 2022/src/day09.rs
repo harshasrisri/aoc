4b0b02dbbdffaa517c9ed94e69dfedcea92dfd9b
Sriharsha Mucheli <harshasrisri@gmail.com>
2022-12-09 01:11:43 -0800

    Day 9 part 2 dirty

--- 2022/src/day09.rs
+++ 2022/src/day09.rs
@@ -1,47 +1,66 @@
 use std::collections::HashSet;
-
 use sscanf::sscanf;
 
-#[derive(Default)]
+#[derive(Default, Debug, Clone, Hash, Eq, PartialEq)]
+struct Point {
+    x: isize,
+    y: isize,
+}
+
+#[derive(Default, Clone, Debug)]
 struct Rope {
-    head: (isize, isize),
-    tail: (isize, isize),
+    points: Vec<Point>,
 }
 
 impl Rope {
-    fn move_head(&mut self, dir: char) -> Option<(isize, isize)> {
-        self.head = match dir {
-            'L' => (self.head.0 + 1, self.head.1),
-            'R' => (self.head.0 - 1, self.head.1),
-            'U' => (self.head.0, self.head.1 + 1),
-            'D' => (self.head.0, self.head.1 - 1),
-            x => panic!("Invalid dir {x}"),
+    fn new(points: usize) -> Rope {
+        Rope { points: vec![ Point::default(); points ] }
+    }
+
+    fn move_head(&mut self, dir: char) -> Point {
+        let head = &self.points[0];
+        self.points[0] = match dir {
+            'L' => Point { x: head.x + 1, y: head.y },
+            'R' => Point { x: head.x - 1, y: head.y },
+            'U' => Point { x: head.x    , y: head.y + 1 },
+            'D' => Point { x: head.x    , y: head.y - 1 },
+            x => panic!("Invalid dir {x}") ,
         };
-        self.tail = match (self.head.0 - self.tail.0, self.head.1 - self.tail.1) {
-            (0, 0) => self.tail,
-            (1, 0) => self.tail,
-            (-1, 0) => self.tail,
-            (0, 1) => self.tail,
-            (0, -1) => self.tail,
-            (1, 1) => self.tail,
-            (-1, -1) => self.tail,
-            (1, -1) => self.tail,
-            (-1, 1) => self.tail,
-            (-1, 2) => (self.tail.0 - 1, self.tail.1 + 1),  // 11'o clock
-            (0, 2) => (self.tail.0, self.tail.1 + 1),       // 12'o clock
-            (1, 2) => (self.tail.0 + 1, self.tail.1 + 1),   // 01'o clock
-            (2, 1) => (self.tail.0 + 1, self.tail.1 + 1),   // 02'o clock
-            (2, 0) => (self.tail.0 + 1, self.tail.1),       // 03'o clock
-            (2, -1) => (self.tail.0 + 1, self.tail.1 - 1),  // 04'o clock
-            (1, -2) => (self.tail.0 + 1, self.tail.1 - 1),  // 05'o clock
-            (0, -2) => (self.tail.0, self.tail.1 - 1),      // 06'o clock
-            (-1, -2) => (self.tail.0 - 1, self.tail.1 - 1), // 07'o clock
-            (-2, -1) => (self.tail.0 - 1, self.tail.1 - 1), // 08'o clock
-            (-2, 0) => (self.tail.0 - 1, self.tail.1),      // 09'o clock
-            (-2, 1) => (self.tail.0 - 1, self.tail.1 + 1),  // 10'o clock
-            x => panic!("Invalid distance {},{}", x.0, x.1),
-        };
-        Some(self.tail)
+
+        let mut head = &self.points[0];
+        for i in 1..self.points.len() {
+            let tail = &self.points[i];
+            self.points[i] = match (head.x - tail.x, head.y - tail.y) {
+                (0, 0) => Point { x: tail.x, y: tail.y},
+                (1, 0) => Point { x: tail.x, y: tail.y},
+                (-1, 0) => Point { x: tail.x, y: tail.y},
+                (0, 1) => Point { x: tail.x, y: tail.y},
+                (0, -1) => Point { x: tail.x, y: tail.y},
+                (1, 1) => Point { x: tail.x, y: tail.y},
+                (-1, -1) => Point { x: tail.x, y: tail.y},
+                (1, -1) => Point { x: tail.x, y: tail.y},
+                (-1, 1) => Point { x: tail.x, y: tail.y},
+                (-1, 2) => Point { x: tail.x - 1, y: tail.y + 1},  // 11'o clock
+                (0, 2) => Point { x: tail.x, y: tail.y + 1},       // 12'o clock
+                (1, 2) => Point { x: tail.x + 1, y: tail.y + 1},   // 01'o clock
+                (2, 2) => Point { x: tail.x + 1, y: tail.y + 1},
+                (2, 1) => Point { x: tail.x + 1, y: tail.y + 1},   // 02'o clock
+                (2, 0) => Point { x: tail.x + 1, y: tail.y},       // 03'o clock
+                (2, -1) => Point { x: tail.x + 1, y: tail.y - 1},  // 04'o clock
+                (2, -2) => Point { x: tail.x + 1, y: tail.y - 1},
+                (1, -2) => Point { x: tail.x + 1, y: tail.y - 1},  // 05'o clock
+                (0, -2) => Point { x: tail.x, y: tail.y - 1},      // 06'o clock
+                (-1, -2) => Point { x: tail.x - 1, y: tail.y - 1}, // 07'o clock
+                (-2, -2) => Point { x: tail.x - 1, y: tail.y - 1},
+                (-2, -1) => Point { x: tail.x - 1, y: tail.y - 1}, // 08'o clock
+                (-2, 0) => Point { x: tail.x - 1, y: tail.y},      // 09'o clock
+                (-2, 1) => Point { x: tail.x - 1, y: tail.y + 1},  // 10'o clock
+                (-2, 2) => Point { x: tail.x - 1, y: tail.y + 1},
+                x => panic!("Invalid distance {},{}", x.0, x.1),
+            };
+            head = &self.points[i];
+        }
+        self.points.last().cloned().unwrap()
     }
 }
 
@@ -54,28 +73,36 @@ pub fn run(input: &'static str) -> (usize, usize) {
         })
         .collect();
 
-    let mut rope = Rope::default();
-    let tail_moves = head_moves
+    let mut rope = Rope::new(2);
+    let p1 = head_moves
         .as_str()
         .chars()
-        .filter_map(|dir| rope.move_head(dir))
+        .map(|dir| rope.move_head(dir))
         .collect::<HashSet<_>>()
         .len();
 
-    (tail_moves, 0)
+    let mut long_rope = Rope::new(10);
+    let p2 = head_moves
+        .as_str()
+        .chars()
+        .map(|dir| long_rope.move_head(dir))
+        .collect::<HashSet<_>>()
+        .len();
+
+    (p1, p2)
 }
 
 #[test]
 fn test() {
     let input = "\
-R 4
-U 4
-L 3
-D 1
-R 4
-D 1
-L 5
-R 2
+R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20
 ";
-    assert_eq!(run(input), (13, 0));
+    assert_eq!(run(input), (88, 36));
 }
