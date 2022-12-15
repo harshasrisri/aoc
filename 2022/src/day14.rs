c26aa334798726fe4b4d0a16776836b7b1cf4935
Sriharsha Mucheli <harshasrisri@gmail.com>
2022-12-14 22:02:54 -0800

    Day 14 part 2 incomplete

--- 2022/src/day14.rs
+++ 2022/src/day14.rs
@@ -56,10 +56,12 @@ impl Line {
 
 struct Map {
     occupied: HashSet<Point>,
+    floor: usize,
 }
 
 impl Map {
     fn from_str(input: &str) -> Self {
+        let mut floor = 0;
         let occupied = input
             .lines()
             .flat_map(|line| {
@@ -67,8 +69,17 @@ impl Map {
                     Line::from_ends(Point::from_str(p1), Point::from_str(p2)).points()
                 })
             })
+            .inspect(|p| floor = floor.max(p.y))
             .collect();
-        Map { occupied }
+        Map { occupied, floor: floor + 2 }
+    }
+
+    fn contains(&self, p: &Point, floor: bool) -> bool {
+        if floor && p.y == self.floor {
+            true
+        } else {
+            self.occupied.contains(p)
+        }
     }
 
     fn abysmal(&self, p: &Point) -> bool {
@@ -79,10 +90,13 @@ impl Map {
             .all(|m| m.y < p.y)
     }
 
-    fn add_sand(&mut self) -> Option<Point> {
+    fn add_sand(&mut self, floor: bool) -> Option<Point> {
         let mut new_sand = Point { x: 500, y: 0 };
+        if self.contains(&new_sand, floor) {
+            return None;
+        }
         loop {
-            // eprintln!("Releasing sand at {:?}", new_sand);
+            // eprintln!("new sand at {:?}", new_sand);
             new_sand = match (new_sand.bot_left(), new_sand.below(), new_sand.bot_right()) {
                 (None, _, _) => {
                     // eprintln!("Falling off the left edge");
@@ -91,20 +105,20 @@ impl Map {
                 (Some(bl), bot, br) => {
                     match *[&bl, &bot, &br]
                         .iter()
-                        .map(|p| self.occupied.contains(p))
+                        .map(|p| self.contains(p, floor))
                         .collect::<Vec<_>>()
                         .as_slice()
                     {
                         [true, true, true] => {
                             // final resting point
-                            // eprintln!("Landed at {:?}", new_sand);
+                            eprintln!("Landed at {:?}", new_sand);
                             self.occupied.insert(new_sand.clone());
                             return Some(new_sand);
                         }
                         [false, true, _] => bl,    // continue left
                         [true, true, false] => br, // continue right
                         [_, false, _] => {
-                            if self.abysmal(&bot) {
+                            if !floor && self.abysmal(&bot) {
                                 // eprintln!("abysmal point: {:?}", new_sand);
                                 return None;
                             } else {
@@ -119,14 +133,35 @@ impl Map {
             }
         }
     }
+
+    fn print(&self, floor: bool) {
+        let (xmn, xmx, ymn, ymx) = self.occupied
+            .iter()
+            .fold((usize::MAX, usize::MIN, usize::MAX, usize::MIN), |(xmn, xmx, ymn, ymx), p| {
+                (xmn.min(p.x), xmx.max(p.x), ymn.min(p.y), ymx.max(p.y))
+            });
+        let map = (xmn..=xmx)
+            .into_iter()
+            .cartesian_product((ymn..=ymx).into_iter())
+            .map(|(x, y)| if self.contains(&Point{x, y}, floor) { 'O' } else { '.' })
+            .collect::<Vec<char>>()
+            .chunks(xmx - xmn)
+            .map(|ch| ch.into_iter().collect::<String>())
+            .join("\n");
+        eprintln!("{map}");
+    }
 }
 
 pub fn run(input: &'static str) -> (usize, usize) {
     let mut bitmap = Map::from_str(input);
     // bitmap.occupied.iter().for_each(|p| eprintln!("{:?}", p));
-    let p1 = (1..).take_while(|_| bitmap.add_sand().is_some()).count();
+    let p1 = (1..).take_while(|_| bitmap.add_sand(false).is_some()).count();
+    bitmap.print(false);
+    eprintln!("======= Part 2 =======");
+    let p2 = (1..).take_while(|_| bitmap.add_sand(true).is_some()).count();
+    // bitmap.print(true);
 
-    (p1, 0)
+    (p1, p2)
 }
 
 #[test]
@@ -135,5 +170,5 @@ fn test() {
 498,4 -> 498,6 -> 496,6
 503,4 -> 502,4 -> 502,9 -> 494,9
 ";
-    assert_eq!(run(input), (24, 0));
+    assert_eq!(run(input), (24, 93));
 }
