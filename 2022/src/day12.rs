ef45fcb13f58f9be27a2a93e5e9b4f09affa8bdc
Sriharsha Mucheli <harshasrisri@gmail.com>
2022-12-17 23:40:32 -0800

    Day 12 incomplete

--- 2022/src/day12.rs
+++ 2022/src/day12.rs
@@ -1,41 +1,39 @@
-use itertools::Itertools;
-
-fn traverse(grid: &Vec<Vec<char>>, row: usize, col: usize) -> Option<usize> {
-    if grid[row][col] == 'E' {
+fn traverse(grid: &Vec<Vec<isize>>, row: usize, col: usize) -> Option<usize> {
+    if grid[row][col] == 27 {
         return Some(1);
     }
 
+    let (r, c) = (row as isize, col as isize);
     let (rm, cm) = (grid.len() as isize, grid[0].len() as isize);
-    let mut min_dist = None;
-
-    let neighbours = [-1, 0, 1]
+    let neighbours = [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]
         .into_iter()
-        .cartesian_product([-1, 0, 1].into_iter())
-        .filter(|(r, c)| (r, c) != (&0, &0))
-        .map(|(r, c)| (row as isize + r, col as isize + c))
-        .filter(|(r, c)| r >= &0 || c >= &0 || r < &rm || c < &cm)
-        .inspect(|(r, c)| eprintln!("{r},{c}"))
-        .map(|(r, c)| (r as usize, c as usize))
+        .filter_map(|(r, c)| if r >= 0 && r < rm && c >= 0 && c < cm {
+            Some((r as usize, c as usize))
+        } else {
+            None
+        })
         .collect::<Vec<_>>();
 
-    for (r, c) in neighbours.into_iter() {
-        eprintln!("{row},{col} - neighbour: {r},{c}");
-        if grid[r][c] as usize - grid[row][col] as usize != 1 {
+    let mut min_dist: Option<usize> = None;
+    for (r, c) in neighbours {
+        if ![0, 1].contains(&(grid[r][c] - grid[row][col])) {
             continue;
         }
-        let dist = traverse(grid, r, c);
-        min_dist = match (min_dist, dist) {
-            (m, None) => m,
-            (None, d) => d,
+        eprintln!("Cell[{row}][{col}] ({}) - neighbour[{r}][{c}] ({})", grid[row][col], grid[r][c]);
+        min_dist = match (min_dist, traverse(grid, r, c)) {
             (Some(m), Some(d)) => Some(m.min(d)),
+            (Some(m), None) => Some(m),
+            (None, Some(d)) => Some(d),
+            (None, None) => None,
         }
     }
+
     min_dist
 }
 
 pub fn run(input: &'static str) -> (usize, usize) {
     let (mut r, mut c) = (0, 0);
-    let grid: Vec<Vec<char>> = input
+    let grid: Vec<Vec<_>> = input
         .lines()
         .enumerate()
         .map(|(row, line)| {
@@ -43,10 +41,14 @@ pub fn run(input: &'static str) -> (usize, usize) {
                 .chars()
                 .enumerate()
                 .map(|(col, ch)| {
-                    if ch == 'S' {
-                        (r, c) = (row, col);
+                    match ch {
+                        'S' => {
+                            (r, c) = (row, col);
+                            -1
+                        }
+                        'E' => 26,
+                        ch => ch as isize - 'a' as isize,
                     }
-                    ch
                 })
                 .collect()
         })
