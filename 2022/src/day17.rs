c0ffed62efa061a52246ab7b2e91e3026057576d
Sriharsha Mucheli <harshasrisri@gmail.com>
2023-12-01 18:26:57 -0800

    Day 17 incomplete

--- 2022/src/day17.rs
+++ 2022/src/day17.rs
@@ -28,58 +28,6 @@ impl Directions {
     }
 }
 
-struct Chamber {
-    space: HashSet<Point>,
-    width: usize,
-    tallest: usize,
-}
-
-impl Chamber {
-    fn new(width: usize) -> Self {
-        Chamber {
-            space: HashSet::new(),
-            width,
-            tallest: 0,
-        }
-    }
-
-    fn contains(&self, point: Point) -> bool {
-        point.row == 0
-            || point.col == 0
-            || point.col == self.width + 1
-            || self.space.contains(&point)
-    }
-
-    fn insert(&mut self, shape: Shape) {
-        let shape_tallest = shape
-            .points
-            .iter()
-            .map(|Point { row, col: _ }| row)
-            .max()
-            .unwrap();
-        self.tallest = self.tallest.max(*shape_tallest);
-        self.space.extend(shape.points);
-    }
-
-    fn print(&self, shape: &Shape) {
-        for row in (0..self.tallest + 7).rev() {
-            for col in 0..=8 {
-                let ch = match (row, col) {
-                    (0, 0) | (0, 8) => '+',
-                    (0, _) => '-',
-                    (_, 0) | (_, 8) => '|',
-                    (row, col) if shape.points.contains(&Point { row, col }) => '@',
-                    (row, col) if self.space.contains(&Point { row, col }) => '#',
-                    _ => '.',
-                };
-                eprint!("{ch}");
-            }
-            eprintln!();
-        }
-        eprintln!();
-    }
-}
-
 #[derive(Clone, PartialEq, Sequence)]
 enum ShapeType {
     HBar,
@@ -231,14 +179,93 @@ impl Shape {
     }
 }
 
-pub fn run(input: &'static str) -> (usize, usize) {
+struct Chamber {
+    space: HashSet<Point>,
+    width: usize,
+    tallest: usize,
+    lowest: usize,
+    insertions: usize,
+}
+
+impl Chamber {
+    fn new(width: usize) -> Self {
+        Chamber {
+            space: HashSet::new(),
+            width,
+            tallest: 0,
+            lowest: 0,
+            insertions: 0,
+        }
+    }
+
+    fn contains(&self, point: Point) -> bool {
+        point.row == 0
+        || point.col == 0
+        || point.col == self.width + 1
+        || self.space.contains(&point)
+    }
+
+    fn insert(&mut self, shape: Shape) {
+        let shape_tallest = shape
+            .points
+            .iter()
+            .map(|Point { row, col: _ }| row)
+            .max()
+            .unwrap();
+        self.tallest = self.tallest.max(*shape_tallest);
+        self.space.extend(shape.points);
+        self.insertions += 1;
+        if self.insertions % 1000000 == 0 {
+            self.do_compaction();
+        }
+    }
+
+    fn print(&mut self, shape: Option<&Shape>) {
+        eprintln!("\x1b[H\x1b[2J");
+        self.do_compaction();
+        for row in (self.lowest..self.tallest + 7).rev() {
+            eprint!("{:20} ", row);
+            for col in 0..=8 {
+                let ch = match (row, col) {
+                    (0, 0) | (0, 8) => '+',
+                    (0, _) => '-',
+                    (_, 0) | (_, 8) => '|',
+                    (row, col) if shape.is_some() && shape.unwrap().points.contains(&Point { row, col }) => '@',
+                    (row, col) if self.space.contains(&Point { row, col }) => '#',
+                    _ => '.',
+                };
+                eprint!("{ch}");
+            }
+            eprintln!();
+        }
+        eprintln!();
+        std::thread::sleep(std::time::Duration::from_millis(10));
+    }
+
+    fn do_compaction(&mut self) {
+        self.lowest = (1..=self.width)
+            .map(|col| {
+                (0..=self.tallest)
+                    .rev()
+                    .find(|&row| self.space.contains(&Point { row, col }))
+                    .unwrap_or_default()
+            })
+            .min()
+            .unwrap();
+
+        self.space.retain(|Point { row, col: _ }| row >= &self.lowest);
+        eprintln!("New dimensions: lowest: {:20}, tallest: {:20}", self.lowest, self.tallest);
+    }
+}
+
+fn run_part(input: &'static str, iters: usize) -> usize {
     let mut directions = Directions::from(input);
     let mut chamber = Chamber::new(7);
 
-    for shape_type in all::<ShapeType>().cycle().take(2022) {
+    for shape_type in all::<ShapeType>().cycle().take(iters) {
         let mut shape = Shape::from_type_in_chamber(shape_type, &chamber);
         // shape.print();
-        // chamber.print(&shape);
+        // chamber.print(Some(&shape));
         for dir in directions.by_ref() {
             shape = match dir {
                 Directions::Left => shape.move_left(&chamber),
@@ -251,14 +278,19 @@ pub fn run(input: &'static str) -> (usize, usize) {
                     }
                 },
             };
-            // chamber.print(&shape);
+            // chamber.print(Some(&shape));
         }
     }
-    (chamber.tallest, 0)
+    chamber.print(None);
+    chamber.tallest
+}
+
+pub fn run(input: &'static str) -> (usize, usize) {
+    (run_part(input, 2022), run_part(input, 5 * input.trim().len())) //run_part(input, 1000000000000))
 }
 
 #[test]
 fn test() {
     let input = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>";
-    assert_eq!(run(input), (3068, 0));
+    assert_eq!(run(input), (3068, 1514285714288));
 }
