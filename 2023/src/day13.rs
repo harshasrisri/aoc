ff45cbe1e0a6ac203941fe065ab510a3b916c925
Sriharsha Mucheli <harshasrisri@gmail.com>
2023-12-29 11:02:35 -0800

    Day 13 part 2

--- 2023/src/day13.rs
+++ 2023/src/day13.rs
@@ -1,21 +1,36 @@
-use itertools::Itertools;
-
 pub fn run(input: &'static str) -> (usize, usize) {
-    let p1 = input.trim()
+    let mirrors = input.trim()
         .split("\n\n")
-        .map(|matrix| matrix.lines().map(|line| line.chars().collect_vec()).collect_vec())
+        .map(|matrix| matrix.lines().map(|line| line.chars().collect::<Vec<_>>()).collect::<Vec<_>>())
         .map(|matrix| (matrix.clone(), transpose(matrix)))
-        .map(|(m, t)| (axis(&m), axis(&t)))
+        .collect::<Vec<_>>();
+
+    let p1 = mirrors.iter()
+        .map(|(m, t)| (axis(m), axis(t)))
         .map(|(x, y)| x.unwrap_or_default() * 100 + y.unwrap_or_default())
         .sum();
 
-    (p1, 0)
+    let p2 = mirrors.iter()
+        .map(|(m, t)| (axis_2(m), axis_2(t)))
+        .map(|(x, y)| x.unwrap_or_default() * 100 + y.unwrap_or_default())
+        .sum();
+
+    (p1, p2)
 }
 
 fn axis(matrix: &[Vec<char>]) -> Option<usize> {
     (1..matrix.len()).position(|row| (0..row).rev().zip(row..matrix.len()).all(|(up, down)| matrix[up] == matrix[down])).map(|row| row + 1)
 }
 
+fn axis_2(matrix: &[Vec<char>]) -> Option<usize> {
+    let row_diff = |up: &[char], down: &[char]| -> usize { up.iter().zip(down.iter()).filter(|(u, d)| u != d).count() };
+    (1..matrix.len()).position(|row| 
+            (0..row).rev().zip(row..matrix.len())
+                .map(|(up, down)| row_diff(&matrix[up], &matrix[down]))
+                .sum::<usize>() == 1
+        )
+        .map(|row| row + 1)
+}
 fn transpose(matrix: Vec<Vec<char>>) -> Vec<Vec<char>> {
     let ret = vec![Vec::new(); matrix[0].len()];
     matrix.into_iter().fold(ret, |mut v, row| {
@@ -43,5 +58,5 @@ fn test1() {
 ..##..###
 #....#..#
 ";
-    assert_eq!(run(input), (405, 0));
+    assert_eq!(run(input), (405, 400));
 }
